814c82b48d1a2952fc48a817514efea2f85e255c
MESHACK CONSTANT <constantmeshack@gmail.com>
2026-02-28 14:16:12 -0800

    Remove .env for secure deploy

--- app.py
+++ app.py
@@ -776,7 +776,6 @@ def paystack_webhook():
 
     return "OK", 200
    
-
 # ----------------- Run App -----------------
 if __name__ == "__main__":
        app.run(host="0.0.0.0", port=5000, debug=False)
