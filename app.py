c413870570ea52e894152ecd48f2f40ec6f9aa24
MESHACK CONSTANT <constantmeshack@gmail.com>
2026-03-17 02:15:07 +0300

    update app.py

--- app.py
+++ app.py
@@ -1259,6 +1259,15 @@ def sitemap():
 
     return Response(sitemap_xml, mimetype="application/xml")
 
+from flask import Flask, render_template, send_from_directory
+import os
+
+app = Flask(__name__)
+
+# Serve the sw.js file
+@app.route('/sw.js')
+def service_worker():
+    return send_from_directory(os.path.dirname(os.path.abspath(__file__)), 'sw.js')
 # ----------------- Run App -----------------
 if __name__ == "__main__":
        app.run(host="0.0.0.0", port=5000, debug=False)
